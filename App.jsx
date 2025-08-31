00907805bfdcdf69148130b930230c1eb7907e28
VANI419 <mvani0729@gmail.com>
2025-08-31 18:36:16 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,43 +1,14 @@
- 
+
 import React from 'react'
-import ClickEvent from './ClickEvent'
-import FormExample from './FormExample'
-import  './App.css'
-import SampleArray from './SampleArray'
-import SampleArrayObjects from './SampleArrayObjects'
-import NestedArrayObject from './NestedArrayObject'
-import TempLit from './TempLit'
-import UserPage from './UserPage'
-import EMployee from './EMployee'
-import CrudApp from './CrudApp'
-import NameCrudApp from './NameCrudApp'
-import image from './image'
-import Apps from './Apps'
-import Animal from './Animal'
-import Plants from './plants'
+import './App.css'
+import MainPage from './shoppingfolder/pages/MainPage'
 
-
-const App = () =>{
-  return(
+const App = () => {
+  return (
     <div>
-      <h1>Welcome to The react</h1>
-      <h1>Hiii</h1>
-      {/* <FormExample />  */}
-    {/* <SampleArray /> */}
-
- {/*<SampleArrayObjects />*/} 
-{ /*<NestedArrayObject />  */}
- { /* <TempLit />  */}
-{/* <ClickEvent />*/}
- {/*<UserPage /> */}
-  <EMployee />   
-{/* <CrudApp />  */}
-{/*  <NameCrudApp />*/} 
-  {/*<image />*/} 
-{/* <Apps /> */}
- {/* <Animal /> */} 
- <Plants /> 
-
+      
+      <MainPage /> 
+     
     </div>
   )
 }
